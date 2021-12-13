aa28e7ade1f1f6813d68467fa3976fd9e97d7c38
Lybrica <karl.miikael100@gmail.com>
2021-12-13 21:37:19 +0200

    [remove] - useless imports

--- src/main/java/com/example/endofgame/service/CategoryService.java
+++ src/main/java/com/example/endofgame/service/CategoryService.java
@@ -10,10 +10,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import javax.transaction.Transactional;
 import java.util.List;
 import java.util.Optional;
-import java.util.stream.Collectors;
 
 @Service
 @Slf4j
