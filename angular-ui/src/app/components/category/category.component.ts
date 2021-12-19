bcfa1a9fe41aea3e5307fcc0735f593dab4e3212
Lybrica <karl.miikael100@gmail.com>
2021-12-19 13:56:04 +0200

    [change] - better looking table of categories

--- angular-ui/src/app/components/category/category.component.ts
+++ angular-ui/src/app/components/category/category.component.ts
@@ -7,15 +7,23 @@ import {Category} from "../../models/category";
   templateUrl: './category.component.html',
   styleUrls: ['./category.component.css']
 })
+
 export class CategoryComponent implements OnInit {
 
   categories: Array<Category> = [];
+  displayedColumns: string[] = ["id", "name", "created", "updated"];
+  dataSource = this.categories;
+
 
   constructor(private categoryService: CategoryService) { }
 
   ngOnInit(): void {
     this.categoryService.readCategoriesFromServer()
-      .subscribe(categoriesFromServer => this.categories = categoriesFromServer);
+      .subscribe(categoriesFromServer => {
+        this.categories = categoriesFromServer
+        this.dataSource = this.categories;
+        console.log(`data received from server, number of elements is: [${categoriesFromServer.length}]`)
+      });
   }
 
 }
